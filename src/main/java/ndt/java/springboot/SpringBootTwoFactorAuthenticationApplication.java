1061de378ad500a4354975a18337fcd387b83f85
dinhthien2000 <you@example.com>
2024-01-24 00:45:20 +0700

    First commit, create and config project

---
+++ src/main/java/ndt/java/springboot/SpringBootTwoFactorAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package ndt.java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwoFactorAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwoFactorAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ndt/java/springboot/SpringBootTwoFactorAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package ndt.java.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTwoFactorAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
